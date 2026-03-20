8dfa57370ac7f1cc8141e19347907f5ca89f10cb
piyush00868 <pk2857636@gmail.com>
2026-03-20 10:43:01 +0530

    fix: case sensitive import

--- FRONTEND/src/app.routes.jsx
+++ FRONTEND/src/app.routes.jsx
@@ -2,7 +2,7 @@ import {createBrowserRouter} from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Home from "./features/interview/pages/Home";
-import Interview from "./features/interview/pages/interview";
+import Interview from "./features/interview/pages/Interview";
 import Protected from "./features/auth/components/protected";
 const router = createBrowserRouter([
 {
