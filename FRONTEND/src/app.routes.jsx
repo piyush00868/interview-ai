9ca3f939e991cd05b653d40570c03aedd758f1f1
piyush00868 <pk2857636@gmail.com>
2026-03-20 21:19:40 +0530

    fix: case-sensitive import issue

--- FRONTEND/src/app.routes.jsx
+++ FRONTEND/src/app.routes.jsx
@@ -3,7 +3,7 @@ import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Home from "./features/interview/pages/Home";
 import Interview from "./features/interview/pages/Interview";
-import Protected from "./features/auth/components/protected";
+import Protected from "./features/auth/components/Protected";
 const router = createBrowserRouter([
 {
     path: "/login",
